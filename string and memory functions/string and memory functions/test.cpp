ae71e62c7f42317c929a1090e8f9d8fbebb9f54e
JulianKevin <marylee06502@gmail.com>
2025-04-02 23:35:44 +0800

    string and memory functions

---
+++ string and memory functions/string and memory functions/test.cpp
@@ -0,0 +1,44 @@
+#define _CRT_SECURE_NO_WARNINGS
+
+#include <stdio.h>
+#include <string.h>
+#include <ctype.h>
+
+
+//字符分类函数
+
+//int main()
+//{
+//	char ch = 'x';
+//	if (islower(ch))
+//	{
+//		printf("小写\n");
+//	}
+//	else
+//	{
+//		printf("非小写\n");
+//	}
+//	return 0;
+//}
+
+//int main()
+//{
+//	int ret = islower('a');
+//	printf("%d\n", ret);
+//	return 0;
+//}
+
+
+//字符转换函数
+//tolower tooupper
+
+//int main()
+//{
+//	int ret = toupper('a');
+//	printf("%c\n", ret);
+//	ret = tolower(ret);
+//	printf("%c\n", ret);
+//
+//	return 0;
+//}
+
